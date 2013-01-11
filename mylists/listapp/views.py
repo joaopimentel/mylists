f33db776565049b0fdf529d14b05ce775fbc13c7
João Pimentel <mail@joaopimentel.net>
2013-01-11 23:49:51 +0000

    added category_detail view, lists links for given tag

--- mylists/listapp/views.py
+++ mylists/listapp/views.py
@@ -1,6 +1,20 @@
+from django.http import HttpResponse
+from django.template import loader, RequestContext
 from django.views.generic import ListView
 from listapp.models import Link
 
 
 class LinkList(ListView):
     model = Link
+
+
+def category_detail(request, tag=None):
+    """Lists all Links tagged with given category.
+    """
+    link_list = Link.objects.select_related('category')\
+                .filter(category__tag=tag)
+    t = loader.get_template('listapp/link_list.html')
+    c = RequestContext(request, {
+            'object_list': link_list,
+    })
+    return HttpResponse(t.render(c))
--- mylists/mylists/urls.py
+++ mylists/mylists/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
 
-from listapp.views import LinkList
+from listapp.views import LinkList, category_detail
 
 admin.autodiscover()
 
@@ -11,4 +11,6 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 
     url(r'^$', LinkList.as_view()),
+
+    url(r'^tag/(?P<tag>\w+)/$', category_detail),
 )
