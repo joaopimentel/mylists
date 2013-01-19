94b0f6f353e070d9aae11d9212817cac86ff2063
João Pimentel <mail@joaopimentel.net>
2013-01-19 18:41:15 +0000

    choose mailbox from userprofileto fetch links

--- mylists/mailfetcher/views.py
+++ mylists/mailfetcher/views.py
@@ -1,13 +1,12 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 
-from models import MailBox
 import utils
 
 
 @login_required
 def get_links(request):
     """View to manually fetch emails."""
-    mailbox = MailBox.objects.all()[0]
+    mailbox = request.user.userprofile.mailbox
     num_links = utils.fetchmails(mailbox)
     return HttpResponse('%d links created.' % num_links)
